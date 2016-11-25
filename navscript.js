e3ccc674e26f29a6b42c8b65183ba85ad5889f68
J-C <jcjoneskern@gmail.com>
2016-11-25 15:56:52 -0500

    testing some stuff

--- navscript.js
+++ navscript.js
@@ -1,19 +1,4 @@
 $(document).ready(function() {
-//hover
-	// $("nav li").mouseover(function() {
-	// 	$(this).fadeTo("fast", .25);
-	// });
-	// $("nav li").mouseleave(function() {
-	// 	$(this).fadeTo("fast", 1);
-	// });
-//click
-	// $("nav li").click(function() {
-	// 	$(this).toggleClass("newbg");
-	// 	$(".newbg").text("Clicked!");
-
-		// $(this).text("Clicked!").css( {"background-color": "#D9BFB3"});
-	// });
-
 //navbar stuff
 	if ($(window).width() > 650 ) {
 			$("#navbutton").css( {"display": "none"});
@@ -30,10 +15,4 @@ $(document).ready(function() {
 	$("#navbutton").click(function() {
 		$(".mobilenav").toggle("fast").removeCss("height");
 	});
-
-
-
-
 }); //closer for .ready()
-
-
